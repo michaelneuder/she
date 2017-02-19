817374961f4a34e8bcb8c7d8b3fdbb99db9f021f
aurbanski <aurbanski27@gmail.com>
2017-02-19 10:47:38 -0700

    Added an about page

--- web_app/she_app/config/routes.rb
+++ web_app/she_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "static_pages/about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
