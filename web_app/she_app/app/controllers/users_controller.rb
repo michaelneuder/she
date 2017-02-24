056683f001a979e81997ab4902b8daf3f70792b6
aurbanski <aurbanski27@gmail.com>
2017-02-23 18:40:10 -0700

    Finished the signup

--- web_app/she_app/app/controllers/users_controller.rb
+++ web_app/she_app/app/controllers/users_controller.rb
@@ -7,6 +7,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def login
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
