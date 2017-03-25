ec2ee0dab4e7769e7a3effbc01564b106ff27bf3
Michael Neuder <michael.neuder@gmail.com>
2017-03-25 16:37:50 -0600

    setting up the mvc for the chat bot
    need to figure out how to send the message to the backend. access this page with localhost:3000/chat/index

--- web_app/she_app/config/routes.rb
+++ web_app/she_app/config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'chat/index'
+  # get '/messageSubmitted.rb'
+
+  get 'welcome/index'
+
+
+  # root 'chat#index'
   root 'static_pages#home'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
   get "/about", to: 'static_pages#about'
+  # get "/chat", to: 'chat/index'
   resources :users
 end
