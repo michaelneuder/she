5d4cef7ed0d94288e4bc0cfa84d7a11d9ee5f23d
aurbanski <aurbanski27@gmail.com>
2017-03-24 20:03:39 -0600

    Tried to set up the twitter shit

---
+++ web_app/she_app/config/initializers/twitter.rb
@@ -0,0 +1,20 @@
+require 'twitter'
+require 'rubygems'
+require 'oauth'
+require 'cleverbot'
+
+$bot = Cleverbot.new('RtW9RmyVVExzC3xi','jpaekuzPDA5HRUNuAqtkOztlErHgsWSN')
+
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['consumer_key']
+  config.consumer_secret     = ENV['consumer_secret']
+  config.access_token        = ENV['access_token']
+  config.access_token_secret = ENV['access_secret']
+end
+
+$consumer_key = OAuth::Consumer.new(
+    ENV['consumer_key'],
+    ENV['consumer_secret'])
+$access_token = OAuth::Token.new(
+    ENV['access_token'],
+    ENV['access_secret'])
