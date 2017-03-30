c0f35b50b09d7a1c4c1bb3f1f9cd813a87141771
bumshakabum <bumcrystlbum@gmail.com>
2017-03-29 22:24:57 -0600

    Added scroll by page by button clicks

--- web_app/she_app/app/assets/javascripts/scroll.js
+++ web_app/she_app/app/assets/javascripts/scroll.js
@@ -1,7 +1,31 @@
 $(document).ready(function(){
-    $(".scroll-button").on('click', function(){
-        if (!window.location.hash) {
-            $()
+    $(".back-to-top").hide();
+    $("#what").on('click', function(e){
+        e.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: $(".block-2").offset().top
+        }, 1000);
+    });
+    $("#how").on('click', function(e){
+        e.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: $(".block-3").offset().top
+        }, 1000);
+    });
+    $("#signup").on('click', function(e){
+        e.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: $(".block-4").offset().top
+        }, 1000);
+    });
+    $(document).scroll(function(){
+        var docScroll = $(document).scrollTop(),
+            almostBottom = $(".block-4").offset().top - 100;
+
+        if(docScroll >= almostBottom) {
+            $(".back-to-top").fadeIn(200);
+        } else {
+            $(".back-to-top").fadeOut(200);
         }
     });
 });
