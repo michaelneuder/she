e4f115983e7c4e78fe9181fd1c9c6c92476ecc83
aurbanski <aurbanski27@gmail.com>
2017-04-03 19:26:05 -0600

    Trying to get tests to work

--- web_app/she_app/test/models/user_test.rb
+++ web_app/she_app/test/models/user_test.rb
@@ -2,23 +2,23 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Fake User", email: "fakkkemail@yahoo.com",
+    @user1 = User.new(name: "Fake User", email: "fakkkemail@yahoo.com",
                       password: "password", password_confirmation: "password",
                       twitter_handle: "AlexUrbanski")
   end
 
   test "should be valid" do
-    assert @user.valid?
+    assert @user1.valid?
   end
 
   test "name should be present" do
-    @user.name = "     "
-    assert_not @user.valid?
+    @user1.name = "     "
+    assert_not @user1.valid?
   end
 
   test "password should be present" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
+    @user1.password = @user1.password_confirmation = " " * 6
+    assert_not @user1.valid?
   end
 
 end
