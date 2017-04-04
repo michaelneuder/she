37cfd7cdd737f3de86884c5ecf39578c697a9ddd
aurbanski <aurbanski27@gmail.com>
2017-04-04 16:43:41 -0600

    Added some tests

--- web_app/she_app/test/models/user_test.rb
+++ web_app/she_app/test/models/user_test.rb
@@ -5,19 +5,36 @@ class UserTest < ActiveSupport::TestCase
     @user1 = User.new(name: "Fake User", email: "fakkkemail@yahoo.com",
                       password: "password", password_confirmation: "password",
                       twitter_handle: "AlexUrbanski")
+    @user2 = User.new(name: "fdsa{$@}", email: "fakkkhoo.com",
+                      password: "pas", password_confirmation: "pass",
+                      twitter_handle: "asd")
   end
 
-  test "should be valid" do
+  test "is_valid" do
     assert @user1.valid?
   end
 
-  test "name should be present" do
+  test "is_not_valid" do
+    assert @user2.valid?
+  end
+
+  test "name_not_present" do
     @user1.name = "     "
     assert_not @user1.valid?
   end
 
-  test "password should be present" do
+  test "name_present" do
+    @user1.name = "Sophie Ekaf"
+    assert_not @user1.valid?
+  end
+
+  test "password_not_present" do
     @user1.password = @user1.password_confirmation = " " * 6
+    assert_not @user1.valid?
+  end
+
+  test "password_present" do
+    @user1.password = @user1.password_confirmation = "paassword"
     assert_not @user1.valid?
   end
 
