4747ea101812f433edbc3eb0f43eaf33789af5de
nzimm <nizi2734@colorado.edu>
2017-04-03 22:00:50 -0600

    updated twilio api files

--- web_app/she_app/app/controllers/twilio_controller.rb
+++ web_app/she_app/app/controllers/twilio_controller.rb
@@ -1,33 +1,13 @@
-require 'twilio-ruby'
+#require 'twilio-ruby'
 
 class TwilioController < ApplicationController
-  include Webhookable
+    #rescue_from Standard Error do |exception|
+    #    trigger_sms_alerts(exception)
+    #end
 
-  after_filter :set_header
-
-  skip_before_action :verify_authenticity_token
-
-  def voice
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
-         r.Play 'http://linode.rabasa.com/cantina.mp3'
-    end
-
-    render_twiml response
-  end
-end
-
-
-rescue_from Standard Error do |exception|
-  trigger_sms_alerts(exception)
-end
-
-
-
-
-def send_message(phone_number, message)
-    twilio_number = ENV['twilioNumber']
-    client = Twilio::Rest::Client.new ENV['twilioAccountSID'], ENV['twilioAuthToken']
+    def send_message(phone_number, message)
+        twilio_number = ENV['twilioNumber']
+        client = Twilio::Rest::Client.new ENV['twilioAccountSID'], ENV['twilioAuthToken']
     
     client.messages.create(
         from:   twilio_number,
