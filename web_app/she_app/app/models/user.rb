085b4291ab751486940642243b4af4cef07de967
aurbanski <aurbanski27@gmail.com>
2017-04-03 19:15:39 -0600

    Fixed creating new user

--- web_app/she_app/app/models/user.rb
+++ web_app/she_app/app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    format: { with: EMAIL_REGEX }#,
+                    # uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  validates :twitter_handle, presence: true, length: { minimum: 1}
 end
--- web_app/she_app/test/models/user_test.rb
+++ web_app/she_app/test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Fake User", email: "fakkkemail@yahoo.com",
+                      password: "password", password_confirmation: "password",
+                      twitter_handle: "AlexUrbanski")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
 end
