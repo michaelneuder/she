17cb9bc051e0969f116ace3df8ea7f7ffc0ee014
Michael Neuder <michael.neuder@gmail.com>
2017-04-12 16:38:24 -0600

    adding some tests
    these are just testing the front end stuff on fb and chat

--- web_app/she_app/test/controllers/facebook_controller_test.rb
+++ web_app/she_app/test/controllers/facebook_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class FacebookControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get facebook_index_url
+    assert_response :success
+  end
+
 end
