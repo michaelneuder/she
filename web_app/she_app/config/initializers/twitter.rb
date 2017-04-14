03b0b7fa8e2e235ece2a9ef27d9374f48c1d7c54
aurbanski <aurbanski27@gmail.com>
2017-04-13 20:45:35 -0600

    Just doing some testing

--- web_app/she_app/config/initializers/twitter.rb
+++ web_app/she_app/config/initializers/twitter.rb
@@ -18,8 +18,8 @@ end
 #$access_token = OAuth::Token.new((ENV['access_token']).to_s, (ENV['access_secret']).to_s)
 
 $consumer_key = OAuth::Consumer.new(
-    "kA7gKxSbjKQ7JXRSHQCnfAMUN",
-    "1t3R52Qp8DfAt1MbXfra9xA51OQrumBfpUBFhAoCckUlQ53pQs")
+  "kA7gKxSbjKQ7JXRSHQCnfAMUN",
+  "1t3R52Qp8DfAt1MbXfra9xA51OQrumBfpUBFhAoCckUlQ53pQs")
 $access_token = OAuth::Token.new(
-    "850774011713925120-lSMiCOx77TSbjpqtfXBPD2KVaRmo24T",
-    "XtoF6sJPK8e1WLlCnL0RiTyAShO2NKv4g4ks3ZEp11YXX")
+  "850774011713925120-lSMiCOx77TSbjpqtfXBPD2KVaRmo24T",
+  "XtoF6sJPK8e1WLlCnL0RiTyAShO2NKv4g4ks3ZEp11YXX")
