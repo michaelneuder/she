1ca75fc33e863d5ab9c1ec3d8c139978b0bdf0c7
aurbanski <aurbanski27@gmail.com>
2017-04-18 19:57:58 -0600

    Sophie follows on creation

--- web_app/she_app/app/controllers/users_controller.rb
+++ web_app/she_app/app/controllers/users_controller.rb
@@ -11,6 +11,31 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in(@user)
+      baseurl = "https://api.twitter.com"
+
+      #Follow the user on signup
+      path = "/1.1/friendships/create.json?screen_name=#{@user.twitter_handle}&follow=true"
+      address = URI("#{baseurl}#{path}")
+      puts address
+      request = Net::HTTP::Post.new address.request_uri
+
+      http = Net::HTTP.new address.host, address.port
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+      request.oauth! http, $consumer_key, $access_token
+      http.start
+      response = http.request request
+
+      puts response
+      puts response.code
+
+
+      messages = nil
+      if response.code == '200'
+        messages = JSON.parse(response.body)
+        puts messages
+      end
       redirect_to signed_up_path
       # client = Twitter::REST::Client.new do |config|
       #   # UNCOMMENT IF API DOESN'T WORK! #
