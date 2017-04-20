a00d7a6d2374a1b9bfef75152f1a7ca217e1579c
aurbanski <aurbanski27@gmail.com>
2017-04-20 17:56:40 -0600

    Fixed some js

--- web_app/she_app/app/assets/javascripts/arrows.js
+++ web_app/she_app/app/assets/javascripts/arrows.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
   console.log('This worked at least');
-  // $('.test').on('click', function(){
-  //   $('.test').css('color', 'red');
-  // });
+
+  //The right arrow code
   $('.right-arrow').on('click', function(){
     $show = $('.is-showing');
     $show.addClass('out');
     if($show.hasClass('second')){
-      $('.right-arrow').addClass('out');
+      $('.right-arrow').children().addClass('out');
     }
     setTimeout(function(){
       $show.addClass('is-hidden');
@@ -16,14 +15,38 @@ $(document).ready(function(){
       $show.next().addClass('is-showing');
       $show.next().removeClass('is-hidden');
       if($show.hasClass('first')){
-        console.log('Clicked on first');
-        $('.left-arrow').addClass('in');
-        $('.left-arrow').addClass('arrow-showing');
-        $('.left-arrow').removeClass('arrow-hidden');
+        $('.left-arrow').children().addClass('in');
+        $('.left-arrow').children().addClass('arrow-showing');
+        $('.left-arrow').children().removeClass('arrow-hidden');
       }
       if($show.hasClass('second')){
-        $('.right-arrow').addClass('arrow-hidden');
-        $('.right-arrow').removeClass('arrow-showing');
+        $('.right-arrow').children().addClass('arrow-hidden');
+        $('.right-arrow').children().removeClass('arrow-showing');
+      }
+    }, 500);
+  });
+
+  //The left arrow code
+  $('.left-arrow').on('click', function(){
+    $show = $('.is-showing');
+    $show.addClass('out');
+    if($show.hasClass('second')){
+      $('.left-arrow').children().addClass('out');
+    }
+    setTimeout(function(){
+      $show.addClass('is-hidden');
+      $show.removeClass('is-showing');
+      $show.prev().addClass('in');
+      $show.prev().addClass('is-showing');
+      $show.prev().removeClass('is-hidden');
+      if($show.hasClass('third')){
+        $('.right-arrow').children().addClass('in');
+        $('.right-arrow').children().addClass('arrow-showing');
+        $('.right-arrow').children().removeClass('arrow-hidden');
+      }
+      if($show.hasClass('second')){
+        $('.left-arrow').children().addClass('arrow-hidden');
+        $('.left-arrow').children().removeClass('arrow-showing');
       }
     }, 500);
   });
