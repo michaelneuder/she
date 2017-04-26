15095ea1adab3b652cf396d4383a0ef7ae464a5e
nzimm <nizi2734@colorado.edu>
2017-04-26 12:03:46 -0600

    added phone_number to schema.rb

--- web_app/she_app/db/schema.rb
+++ web_app/she_app/db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20170329173648) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "phone_number"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
